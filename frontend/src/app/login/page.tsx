c1ecd8de9df5543f4b4bc20f0bcbc1aede1f4f06
Csingh26 <singh.chaittanya@gmail.com>
2024-11-15 10:44:09 +0530

    login-signup-page-basicSetup

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+const LoginPage = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default LoginPage
---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+const SignUpPage = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default SignUpPage
