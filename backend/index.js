9a1c0d9012a126e74ef5db2ab5e17e20d90020dd
Csingh26 <singh.chaittanya@gmail.com>
2024-11-16 10:05:53 +0530

    postgres-and-prisma-setup

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const cookie = require('cookie-parser')
+const prisma = require('./config/primsaClient')
+
+const app = express()
+
+require('dotenv').config({
+    path: '/Users/chaitanyasingh/Documents/Project/13/backend/.env' //configure to your .env file path
+})
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+app.use(bodyParser.json())
+app.use(cookie())
+
+prisma.$connect()
+    .then(() => {
+        console.log('Prisma Client is connected to the database')
+    })
+    .catch((e) => {
+        console.log('Error connecting to the databse', e)
+    })
+
+app.get('/', (req, res) => {
+    res.send('Serveer is running and Prisma is connected')
+})
+
+const PORT = process.env.PORT || 3876
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
