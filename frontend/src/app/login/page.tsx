e425501e493e3a39bffed66cbc65af9641a200b6
Csingh26 <singh.chaittanya@gmail.com>
2024-11-17 19:22:05 +0530

    login-route-setup

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,31 +1,59 @@
 "use client"
 
 import React, { useState } from "react"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const LoginPage: React.FC = () => {
     const[username, setUsername] = useState('')
     const[password, setPassword] = useState('')
-    const[error, setError] = useState('')
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
         if (!username || !password) {
-            setError('Please fill in both fields!')
+            toast.error("Please fill in all the fields!", {
+                position: "top-center"
+            })
             return
         }
-
-        setError('')
     
-        console.log('Login Submitted!', {
-            username,
-            password
-        })
+        try {
+            const response = await fetch("http://localhost:7565/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            })
+
+            const data = await response.json()
+
+            if (response.ok) {
+                localStorage.setItem("token", data.token)
+
+                toast.success("Login successfull", {
+                    position: "bottom-right"
+                })
+            } else {
+                toast.error(data.error || "Login failed!", {
+                    position: "top-center"
+                })
+            }
+        } catch (e) {
+            console.error(e)
+            toast.error("SOmething went wrong. Please try again!", {
+                position: "top-center"
+            })
+        }
     }
     return (
         <div className="flex-justify-center items-center h-screen pt-60">
+            <ToastContainer />
             <form onSubmit={handleSubmit}>
                 <h3 className="text-2xl font-bold text-center">Login</h3>
-                {error && <p className="text-red-500 mb-4">{error}</p>}
 
                 <div className="mb-4">
                     <label htmlFor="username" className="block text-gray-700">Username</label>
